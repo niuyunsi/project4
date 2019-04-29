5852267425393c368510cf384a6943e03f2b94e0
Yunsi Niu <niuyunsi@gmail.com>
2019-04-29 14:15:36 -0700

    63

--- android/app/src/main/java/com/project4/MainApplication.java
+++ android/app/src/main/java/com/project4/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.oblador.vectoricons.VectorIconsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,10 +23,7 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new ReactVideoPackage()
-      );
+      return Arrays.<ReactPackage>asList(new MainReactPackage(), new ReactVideoPackage(), new VectorIconsPackage());
     }
 
     @Override
